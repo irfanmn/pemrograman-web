99a67e5f781e32fe9c51981c1fb49ccf347f14f9
irfanmn <99636194+irfanmn@users.noreply.github.com>
2022-03-09 21:23:51 +0700

    Add files via upload

---
+++ Tugas 1/latihan_global.php
@@ -0,0 +1,11 @@
+<?php
+
+var_dump(1 + 1);
+## output adalah int(2)
+var_dump("1 + 1");
+## output adalah string(5) "1 + 1" string(5) artinya string berisi 5 karakter
+var_dump( 1 + "1");
+## output adalah int(2)
+var_dump( "1" + "1");
+## Output adalah int(2)
+?>
